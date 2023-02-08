1331bed7e96f43360ea8c81d7566ed071d09935c
admin <admin@MacBook-Air-demo-2.local>
2023-02-08 21:40:39 +0300

    CI and CD 1st att

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,12 +1,11 @@
 import os
 from datetime import timedelta
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default='12345')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
