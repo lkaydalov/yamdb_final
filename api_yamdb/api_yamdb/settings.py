9f8e8d3a95175b50bec066c2d1b577b369b173cd
admin <admin@MacBook-Air-demo-2.local>
2023-02-10 09:39:09 +0300

    CI and CD moved project_static to api

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'api_yamdb/project_static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'api_yamdb/api/project_static'),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
